92949c354f4a34545362450d539f369181fb2346
LemmyH <alan-lemmy-hopkins@hotmail.com>
2026-09-11 20:37:11 +0100

    Update service worker to version 7 and refactor code

--- Service-worker.js
+++ Service-worker.js
@@ -1,27 +1,37 @@
-const CACHE='valor-tool-v6';
+const CACHE='valor-tool-v7';
 
-self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>
-      c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])
-    )
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate',e=>{
-  e.waitUntil(
+self.addEventListener('activate',event=>{
+  event.waitUntil(
     caches.keys().then(keys=>
       Promise.all(
-        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+        keys
+          .filter(key=>key!==CACHE)
+          .map(key=>caches.delete(key))
       )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request).then(
+      cached=>cached||fetch(event.request)
+    )
   );
 });
