9d4b335a73c57358c57e6c05c465ef30c0e0ac6e
LemmyH <alan-lemmy-hopkins@hotmail.com>
2026-09-22 17:00:25 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,11 @@
-const CACHE='valor-tool-v8-step2';
+const CACHE='valor-tool-v8.1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(
-    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-  )));
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch',event=>{
