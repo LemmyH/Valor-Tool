ece1540b617900e1bb8b55426edc2c09c7c29e52
LemmyH <alan-lemmy-hopkins@hotmail.com>
2026-09-22 18:17:33 +0100

    Upgrade service worker to version 8.6

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,37 @@
-const CACHE='valor-tool-v8.5';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',event=>{
- event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
- self.skipWaiting();
+const CACHE = 'valor-tool-v8.6';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
-self.addEventListener('activate',event=>{
- event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
- self.clients.claim();
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
-self.addEventListener('fetch',event=>{
- event.respondWith(caches.match(event.request).then(c=>c||fetch(event.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
 });
